f741e1953438efdfd5112009606b92f83a29ece1
Amor Kumar <contact@amorkumar.com>
2023-12-11 00:53:39 -0500

    Scaffold header and footer

--- patterns/footer.php
+++ patterns/footer.php
@@ -2,17 +2,29 @@
 /**
  * Title: Footer
  * Slug: bp/footer
- * Description: Footer with site logo and menus.
+ * Description: Footer with menu and copyright.
  * Categories: footer
  * Block Types: core/template-part/footer
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"48px","bottom":"48px"},"margin":{"top":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"black","textColor":"white","layout":{"type":"constrained"},"fontSize":"x-small"} -->
-<div class="wp-block-group alignfull has-white-color has-black-background-color has-text-color has-background has-link-color has-x-small-font-size" style="margin-top:0;padding-top:48px;padding-bottom:48px"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide"><!-- wp:image {"id":49,"width":60,"height":60,"sizeSlug":"full","linkDestination":"custom"} -->
-<figure class="wp-block-image size-full is-resized"><a href="/"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/logo.png' ) ) ?>" alt="" class="wp-image-49" width="60" height="60"/></a></figure>
-<!-- /wp:image -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0px","bottom":"80px"},"margin":{"top":"0"},"blockGap":"0"},"border":{"radius":"24px"}},"layout":{"type":"constrained"},"fontSize":"x-small","metadata":{"name":"Footer"}} -->
+<div class="wp-block-group alignfull has-x-small-font-size" style="border-radius:24px;margin-top:0;padding-top:0px;padding-bottom:80px"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"120px","bottom":"120px","left":"298px","right":"298px"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"border":{"radius":{"topLeft":"24px","topRight":"24px","bottomLeft":"0px","bottomRight":"0px"}}},"backgroundColor":"black-700","textColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-white-color has-black-700-background-color has-text-color has-background has-link-color" style="border-top-left-radius:24px;border-top-right-radius:24px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;padding-top:120px;padding-right:298px;padding-bottom:120px;padding-left:298px"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontWeight":"600"}},"className":"is-style-body-18"} -->
+<p class="has-text-align-center is-style-body-18" style="font-weight:600">Connect</p>
+<!-- /wp:paragraph -->
 
-<!-- wp:navigation {"ref":26} /--></div>
+<!-- wp:heading {"textAlign":"center","className":"is-style-h1"} -->
+<h2 class="wp-block-heading has-text-align-center is-style-h1">Let's create your next big idea.</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"},"margin":{"bottom":"0px"}},"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"24px","bottomRight":"24px"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"black-700","textColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-white-color has-black-700-background-color has-text-color has-background has-link-color" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:24px;border-bottom-right-radius:24px;margin-bottom:0px;padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"flex","allowOrientation":false,"justifyContent":"center"}} -->
+<div class="wp-block-group alignwide"><!-- wp:navigation {"ref":34,"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|medium"}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontWeight":"400"},"elements":{"link":{"color":{"text":"var:preset|color|black-200"}}}},"textColor":"black-200","className":"is-style-body-14"} -->
+<p class="has-text-align-center is-style-body-14 has-black-200-color has-text-color has-link-color" style="font-weight:400">© 2023 Blink Portfolio</p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
--- patterns/header.php
+++ patterns/header.php
@@ -7,12 +7,10 @@
  * Block Types: core/template-part/header
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"32px","bottom":"32px","right":"24px","left":"24px"},"margin":{"top":"0px"}},"position":{"type":"sticky","top":"0px"}},"className":"site-header","layout":{"type":"constrained"}} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"32px","bottom":"32px","right":"24px","left":"24px"},"margin":{"top":"0px"}},"position":{"type":""}},"className":"site-header","layout":{"type":"constrained"},"metadata":{"name":"Header"}} -->
 <div class="wp-block-group alignfull site-header" style="margin-top:0px;padding-top:32px;padding-right:24px;padding-bottom:32px;padding-left:24px"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"nowrap","orientation":"horizontal"}} -->
-<div class="wp-block-group alignwide"><!-- wp:image {"id":49,"width":60,"sizeSlug":"full","linkDestination":"custom"} -->
-<figure class="wp-block-image size-full is-resized"><a href="/"><img src="<?php echo get_theme_file_uri( 'assets/images/logo.png' ); ?>" alt="" class="wp-image-49" width="60"/></a></figure>
-<!-- /wp:image -->
+<div class="wp-block-group alignwide"><!-- wp:site-logo {"width":30,"shouldSyncIcon":true} /-->
 
-<!-- wp:navigation {"ref":26} /--></div>
+<!-- wp:navigation {"ref":34,"overlayMenu":"always"} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
