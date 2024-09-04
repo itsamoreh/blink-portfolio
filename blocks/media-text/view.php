60ae5d7c18a67f7def0170b89f67d98176bd42fb
Amor Kumar <contact@amorkumar.com>
2024-09-04 10:47:52 -0400

    Add render to block.json, fix errors

--- blocks/media-text/view.php
+++ blocks/media-text/view.php
@@ -2,36 +2,40 @@
 /**
  * Media and Text Block Template.
  *
- * @param   array $block The block settings and attributes.
- * @param   string $content The block inner HTML (empty).
- * @param   bool $is_preview True during backend preview render.
- * @param   int $post_id The post ID the block is rendering content against.
+ * @param array    $attributes     The array of attributes for this block.
+ * @param string   $content        Rendered block output. ie. <InnerBlocks.Content />.
+ * @param WP_Block $block_instance The instance of the WP_Block class that represents the block being rendered.
+ *
+ * @package bp
  */
 
 // Create id attribute allowing for custom "anchor" value.
-$id = 'media-text-' . $block['id'];
-if (!empty($block['anchor'])) {
-    $id = $block['anchor'];
+$id = 'media-text-' . $block_instance->block_type->name;
+if (!empty($attributes['anchor'])) {
+    $id = $attributes['anchor'];
 }
 
 // Create class attribute allowing for custom "className" and "align" values.
-$className = 'wp-block-custom-blocks-media-text';
-if (!empty($block['className'])) {
-    $className .= ' ' . $block['className'];
+$class_name = 'wp-block-bp-media-text';
+if (!empty($attributes['className'])) {
+    $class_name .= ' ' . $attributes['className'];
 }
-if (!empty($block['align'])) {
-    $className .= ' align' . $block['align'];
+if (!empty($attributes['align'])) {
+    $class_name .= ' align' . $attributes['align'];
 }
 
-// Load values and assign defaults.
-$media_url = get_field('media_url') ?: '';
-$media_alt = get_field('media_alt') ?: '';
-$content = get_field('content') ?: '';
+// Load values from attributes
+$media_id = $attributes['mediaId'] ?? 0;
+$media_url = $attributes['mediaUrl'] ?? '';
+$media_alt = $attributes['mediaAlt'] ?? '';
+$content = $attributes['content'] ?? '';
 
 ?>
-<div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
+<div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($class_name); ?>">
     <div class="media-text__media">
-        <?php if ($media_url) : ?>
+        <?php if ($media_id) : ?>
+            <?php echo wp_get_attachment_image($media_id, 'full', false, array('alt' => $media_alt)); ?>
+        <?php elseif ($media_url) : ?>
             <img src="<?php echo esc_url($media_url); ?>" alt="<?php echo esc_attr($media_alt); ?>" />
         <?php endif; ?>
     </div>
