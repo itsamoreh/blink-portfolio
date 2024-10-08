4200fa00e0a58d0d496ae09f2b2804a5c98576f0
Amor Kumar <contact@amorkumar.com>
2024-10-07 23:46:14 -0400

    Tweak

--- blocks/media-text/view.php
+++ blocks/media-text/view.php
@@ -9,37 +9,22 @@
  * @package bp
  */
 
-// Create id attribute allowing for custom "anchor" value.
-$id = 'media-text-' . $block_instance->block_type->name;
-if (!empty($attributes['anchor'])) {
-    $id = $attributes['anchor'];
-}
-
-// Create class attribute allowing for custom "className" and "align" values.
-$class_name = 'wp-block-bp-media-text';
-if (!empty($attributes['className'])) {
-    $class_name .= ' ' . $attributes['className'];
-}
-if (!empty($attributes['align'])) {
-    $class_name .= ' align' . $attributes['align'];
-}
-
 // Load values from attributes
-$media_id = $attributes['mediaId'] ?? 0;
+$media_id  = $attributes['mediaId'] ?? 0;
 $media_url = $attributes['mediaUrl'] ?? '';
 $media_alt = $attributes['mediaAlt'] ?? '';
-$content = $attributes['content'] ?? '';
+$content   = $attributes['content'] ?? '';
 
 ?>
-<div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($class_name); ?>">
-    <div class="media-text__media">
-        <?php if ($media_id) : ?>
-            <?php echo wp_get_attachment_image($media_id, 'full', false, array('alt' => $media_alt)); ?>
-        <?php elseif ($media_url) : ?>
-            <img src="<?php echo esc_url($media_url); ?>" alt="<?php echo esc_attr($media_alt); ?>" />
-        <?php endif; ?>
-    </div>
-    <div class="media-text__content">
-        <?php echo wp_kses_post($content); ?>
-    </div>
+<div <?php echo get_block_wrapper_attributes(['class' => 'bp-media-text']); // phpcs:ignore ?>>
+	<div class="bp-media-text__media">
+		<?php if ( $media_id ) : ?>
+			<?php echo wp_get_attachment_image( $media_id, 'full', false, array( 'alt' => $media_alt ) ); ?>
+		<?php elseif ( $media_url ) : ?>
+			<img src="<?php echo esc_url( $media_url ); ?>" alt="<?php echo esc_attr( $media_alt ); ?>" />
+		<?php endif; ?>
+	</div>
+	<div class="bp-media-text__content">
+		<?php echo wp_kses_post( $content ); ?>
+	</div>
 </div>
--- blocks/post-grid/view.php
+++ blocks/post-grid/view.php
@@ -9,54 +9,42 @@
  * @package bp
  */
 
-// Create id attribute allowing for custom "anchor" value.
-$id = 'post-grid-' . $block_instance->block_type->name;
-if (!empty($attributes['anchor'])) {
-    $id = $attributes['anchor'];
-}
-
-// Create class attribute allowing for custom "className" and "align" values.
-$class_name = 'wp-block-bp-post-grid';
-if (!empty($attributes['className'])) {
-    $class_name .= ' ' . $attributes['className'];
-}
-if (!empty($attributes['align'])) {
-    $class_name .= ' align' . $attributes['align'];
-}
-
 // Load values and assign defaults.
-$number_of_posts = $attributes['numberOfPosts'] ?? 12;
-$post_type = $attributes['postType'] ?? 'post';
+$number_of_posts     = $attributes['numberOfPosts'] ?? 12;
+$post_grid_post_type = $attributes['postType'] ?? 'post';
 
 // WP_Query arguments
 $args = array(
-    'post_type'      => $post_type,
-    'posts_per_page' => $number_of_posts,
+	'post_type'      => $post_grid_post_type,
+	'posts_per_page' => $number_of_posts,
 );
 
 // The Query
-$query = new WP_Query($args);
+$query = new WP_Query( $args );
 
 // The Loop
-if ($query->have_posts()) : ?>
-    <div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($class_name); ?>">
-        <div class="post-grid">
-            <?php while ($query->have_posts()) : $query->the_post(); ?>
-                <article class="post-grid-item">
-                    <?php if (has_post_thumbnail()) : ?>
-                        <div class="post-grid-item-image">
-                            <?php the_post_thumbnail('medium'); ?>
-                        </div>
-                    <?php endif; ?>
-                    <div class="post-grid-item-content">
-                        <h3 class="post-grid-item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                        <div class="post-grid-item-date"><?php echo get_the_date(); ?></div>
-                    </div>
-                </article>
-            <?php endwhile; ?>
-        </div>
-    </div>
-<?php
+if ( $query->have_posts() ) : ?>
+    <div <?php echo get_block_wrapper_attributes(['class' => 'bp-post-grid']); // phpcs:ignore ?>>
+		<div class="bp-post-grid__inner">
+			<?php
+			while ( $query->have_posts() ) :
+				$query->the_post();
+				?>
+				<article class="bp-post-grid__item">
+					<?php if ( has_post_thumbnail() ) : ?>
+						<div class="bp-post-grid__item-image">
+							<?php the_post_thumbnail( 'medium' ); ?>
+						</div>
+					<?php endif; ?>
+					<div class="bp-post-grid__item-content">
+						<h3 class="bp-post-grid__item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+						<div class="bp-post-grid__item-date"><?php echo get_the_date(); ?></div>
+					</div>
+				</article>
+			<?php endwhile; ?>
+		</div>
+	</div>
+	<?php
 endif;
 
 // Restore original Post Data
